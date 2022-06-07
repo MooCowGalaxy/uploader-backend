4ce655c985a4644ada92058217bb4888885b74ba
MooCowGalaxy <moocow@moocow.dev>
2022-06-07 11:35:43 -0700

    migrate to new server

--- index.js
+++ index.js
@@ -1,41 +1,34 @@
 const express = require('express')
 const upload = require('multer')();
-const mysql = require('mysql')
-const Minio = require('minio')
+const redis = require('redis')
+const fs = require('fs')
 
 const config = require('./config.json')
 
 const app = express()
-const minio = new Minio.Client(config.minio)
-const pool = mysql.createPool(config.database)
+const client = redis.createClient()
+client.connect().then()
 
-function query(statement) {
-    return new Promise((resolve, reject) => {
-        pool.getConnection(function (err, connection) {
-            if (err) { reject(err); return }
-            connection.query(statement, function (err, results) {
-                connection.release()
-                if (err) { reject(err); return }
-                resolve(results)
-            })
-        })
-    })
-}
-const escape = mysql.escape
-
-function uploadFile(fileName, file) {
+/* function uploadFile(fileName, file) {
     return new Promise(((resolve, reject) => {
         minio.putObject(config.minioBucket, fileName, file, function(err) {
             if (err) reject(err)
             resolve()
         })
     }))
+} */
+function uploadFile(fileName, file) {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(`/mnt/data/images/${fileName}`, file, () => {
+            resolve()
+        })
+    })
 }
 
-function createTokenString() {
+function createTokenString(length = 6) {
     let base62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890".split('')
     let token = ""
-    for (let i = 0; i < 6; i++) {
+    for (let i = 0; i < length; i++) {
         token = `${token}${base62[Math.floor(Math.random() * base62.length)]}`
     }
     return token
@@ -54,9 +47,21 @@ app.post('/api/upload', upload.single('sharex'), async (req, res) => {
     let fileName = `${fileId}.${extension}`
     await uploadFile(fileName, file.buffer)
 
-    let url = `https://moo.rocks/${fileName}`
+    let url = `https://mooi.ng/${fileName}`
+    let count = await client.get('image-counter')
+    count++;
+    let data = {
+        id: count,
+        fileId,
+        extension,
+        originalName: file.originalname,
+        size: file.size,
+        timestamp: Date.now()
+    }
 
-    await query(`INSERT INTO images (fileId, extension, originalName, size, timestamp) VALUES (${escape(fileId)}, ${escape(extension)}, ${escape(file.originalname)}, ${file.size}, ${Date.now()})`)
+    await client.json.set(`image-${fileId}`, '$', data)
+    await client.json.set(`image-id-${count}`, '$', data)
+    await client.incr('image-counter')
 
     res.json({error: false, message: "Uploaded!", url: url})
 })
@@ -65,11 +70,11 @@ app.get('/:id', async (req, res) => {
     let fileId = fileName.split('.').slice(0, -1).join(".")
     let extension = fileName.split('.').slice(-1)
 
-    let results = await query(`SELECT * FROM images WHERE fileId = ${escape(fileId)} AND extension = ${escape(extension)}`)
-    if (results.length === 0) return res.status(404).send("File not found")
+    let result = await client.json.get(`image-id-${fileId}`)
+    if (result === null) return res.status(404).send("File not found")
 
     res.header("Access-Control-Allow-Origin", "*")
-    res.sendFile(`/mnt/data/i.moocow.dev/${fileName}`)
+    res.sendFile(`/mnt/data/images/${fileName}`)
 })
 app.get('/', async (req, res) => {
     res.send("your mom")
