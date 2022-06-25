d14e23d5cc43cea0852e7eaefeb8cb0a69cc541a
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-06-25 08:47:46 -0700

    table typo

--- tasks/stats.js
+++ tasks/stats.js
@@ -2,7 +2,7 @@ module.exports = (prisma) => {
     return {
         task: () => {
             (async () => {
-                await prisma.$queryRaw`INSERT INTO stats (timestamp, users, bytesUsed, imagesUploaded) VALUES (${Math.round(Date.now() / 1000)}, (SELECT COUNT(*) AS result FROM user), (SELECT SUM(size) AS result FROM image), (SELECT COUNT(*) AS result FROM image))`
+                await prisma.$queryRaw`INSERT INTO Stats (timestamp, users, bytesUsed, imagesUploaded) VALUES (${Math.round(Date.now() / 1000)}, (SELECT COUNT(*) AS result FROM user), (SELECT SUM(size) AS result FROM image), (SELECT COUNT(*) AS result FROM image))`
             })()
         },
         time: '0 * * * *' // cron format
