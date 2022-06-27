a5541bf93b2a4aaf02b6efaec440d481f9327957
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-06-26 21:54:37 -0700

    use React build instead of static

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require("express")
 const {renderFile} = require("../util/functions")
+const config = require('../config.json')
+const path = require('path')
 
 const namespace = '/'
+const buildPath = path.normalize(path.join(__dirname, '../dist/dashboard'))
 
 function getRouter({checkForDomain, getUser}) {
     const mainRouter = express.Router()
@@ -14,19 +17,20 @@ function getRouter({checkForDomain, getUser}) {
     mainRouter.get(/^\/dashboard(\/[a-zA-Z0-9\/]*)?$/, async (req, res) => {
         const user = await getUser(req)
         if (!user) return res.redirect('/auth/login')
-        res.send(await renderFile('dashboard', {user: user.data}))
+        res.sendFile(path.join(buildPath, 'index.html'))
         /* if (!config.whitelist) {
             res.send(await renderFile('dashboard', {user: user.data}))
         } else {
             let result = await prisma.whitelist.findUnique({
                 where: {
-                    id: user.userId
+                    discordId: user.userId
                 }
             })
             if (result.length === 0) res.send(await renderFile('whitelistOnly', {user: user.data}))
             else res.send(await renderFile('dashboard', {user: user.data}))
         } */
     })
+    mainRouter.use(express.static(buildPath))
 
     return mainRouter
 }
