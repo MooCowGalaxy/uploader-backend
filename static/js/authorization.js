e792623251f5dc7cc0828cb8a4dddd34162d3093
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-07-02 12:03:24 -0700

    fix icon not showing up

--- static/js/authorization.js
+++ static/js/authorization.js
@@ -61,13 +61,13 @@ let isDropdownVisible = false;
 function toggleNavbarDropdown() {
     if (!isDropdownVisible) {
         isDropdownVisible = true
-        $('.bi-chevron-down').addClass('hidden')
-        $('.bi-chevron-up').removeClass('hidden')
+        $('#navbar-dropdown-icon').addClass('hidden')
+        $('#navbar-dropdown-icon-focus').removeClass('hidden')
         $('#n-dropdown').removeClass('hidden')
     } else {
         isDropdownVisible = false
-        $('.bi-chevron-down').removeClass('hidden')
-        $('.bi-chevron-up').addClass('hidden')
+        $('#navbar-dropdown-icon').removeClass('hidden')
+        $('#navbar-dropdown-icon-focus').addClass('hidden')
         $('#n-dropdown').addClass('hidden')
     }
 }
@@ -122,11 +122,17 @@ if (document.cookie.length > 0 && document.cookie.includes('token')) {
                         $('#nav-dropdown').addClass('inline-block')
                         $('#nav-dropdown').html(`
                         <button id="navbar-dark" class="rounded-full mr-2 align-middle inline-block no-transition"></button>
-                        <button class="text-white align-middle inline-block" id="navbar-dropdown">${tag} <svg id="navbar-dropdown-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-</svg><svg id="navbar-dropdown-icon-focus" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up hidden" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
-</svg></button>
+                        <button class="text-white align-middle inline-block" id="navbar-dropdown">${tag}
+                        <i id="navbar-dropdown-icon" class="inline-block align-middle mb-0.5">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                            </svg>
+                        </i>
+                        <i id="navbar-dropdown-icon-focus" class="inline-block align-middle mb-0.5 hidden">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
+                            </svg>
+                        </i></button>
                         <div id="n-dropdown" class="origin-top-right absolute right-0 mt-2 w-40 rounded-md shadow-lg py-1 bg-white hidden" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
                             <a href="/dashboard" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300" role="menuitem" tabindex="-1">Dashboard</a>
                             <a href="/auth/logout" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-300" role="menuitem" tabindex="-1">Sign out</a>
