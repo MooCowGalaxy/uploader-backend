3d188af609b7eba0cd910b7c0ac305f19ef897c0
MooCowGalaxy <64293406+MooCowGalaxy@users.noreply.github.com>
2022-07-02 12:04:04 -0700

    increase margin

--- static/js/authorization.js
+++ static/js/authorization.js
@@ -123,12 +123,12 @@ if (document.cookie.length > 0 && document.cookie.includes('token')) {
                         $('#nav-dropdown').html(`
                         <button id="navbar-dark" class="rounded-full mr-2 align-middle inline-block no-transition"></button>
                         <button class="text-white align-middle inline-block" id="navbar-dropdown">${tag}
-                        <i id="navbar-dropdown-icon" class="inline-block align-middle mb-0.5">
+                        <i id="navbar-dropdown-icon" class="inline-block align-middle mb-1">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
                             </svg>
                         </i>
-                        <i id="navbar-dropdown-icon-focus" class="inline-block align-middle mb-0.5 hidden">
+                        <i id="navbar-dropdown-icon-focus" class="inline-block align-middle mb-1 hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/>
                             </svg>
